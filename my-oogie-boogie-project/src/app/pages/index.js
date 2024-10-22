6679ed2bc97fe948db2e0caf86b221cb035b6219
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-10-22 09:27:23 +0800

    update: assets using

--- my-oogie-boogie-project/src/app/pages/index.js
+++ my-oogie-boogie-project/src/app/pages/index.js
@@ -11,12 +11,12 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Oogie Boogie 痛扁聖誕老人</h1>
+      <h1>痛扁聖誕老人</h1>
       <button onClick={handleClick}>扁聖誕老人</button>
       {showGif && (
         // eslint-disable-next-line @next/next/no-img-element
         <img
-          src="/path/to/oogie_boogie.gif"
+          src="my-oogie-boogie-project/src/app/pages/assets/kid-beating-santa.gif"
           alt="Oogie Boogie beating Santa Claus"
         />
       )}
